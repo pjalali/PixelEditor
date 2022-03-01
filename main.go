9b6cc3599fc514a213fb581ae4934be12e2d3061
Paymon Jalali <paymon.jalali@gmail.com>
2022-02-28 20:57:06 -0800

    moving port to env var

--- main.go
+++ main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	routes "pjalali.github.io/pixeleditor/internal/server"
 )
 
-const PORT = ":5000"
-
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		panic("$PORT must be set")
+	}
+
 	fs := http.FileServer(http.Dir("./internal/server/web/static"))
 	http.Handle("/", fs)
 	http.HandleFunc("/capture", routes.Capture)
 	http.HandleFunc("/edit", routes.Edit)
 
-	log.Println("Listening on " + PORT)
-	err := http.ListenAndServe(PORT, nil)
+	log.Println("Listening on " + port)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Println(err)
 	}
